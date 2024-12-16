047fa0896bf88102965affbbe3fd922f3660b1bb
sungyeong98 <am25227903@gmail.com>
2024-12-16 14:41:17 +0900

    feat: 3-12

---
+++ src/main/java/com/mysite/sbb_practice3/CommonUtil.java
@@ -0,0 +1,19 @@
+package com.mysite.sbb_practice3;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+
+}
